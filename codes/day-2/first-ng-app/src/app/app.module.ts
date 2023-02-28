0b462770075fa7cbc191576b632e7b7b0d1d6a4f
Joydip <joydip.mondal@gmail.com>
2023-02-28 17:32:19 +0530

    day-2 end of day upload

--- codes/day-2/first-ng-app/src/app/app.module.ts
+++ codes/day-2/first-ng-app/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculationComponent } from './calculation/calculation.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent, CalculatorComponent
+    AppComponent, CalculatorComponent, CalculationComponent
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- codes/day-2/first-ng-app/src/app/calculation/calculation.component.ts
+++ codes/day-2/first-ng-app/src/app/calculation/calculation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
     selector: 'app-calculation',
@@ -6,5 +7,44 @@ import { Component } from "@angular/core";
     styleUrls: ['./calculation.component.css']
 })
 export class CalculationComponent {
+    result = 0
+    choice = 2
+    calcForm = new FormGroup({
+        add: new FormControl(1),
+        subtract: new FormControl(2),
+        multiply: new FormControl(3),
+        divide: new FormControl(4),
+        first: new FormControl(0),
+        second: new FormControl(0)
+    })
+    setChoice(_choice: number) {
+        this.choice = _choice
+    }
+    get first() {
+        return this.calcForm.get('first')
+    }
+    get second() {
+        return this.calcForm.get('second')
+    }
+    calculate() {
+        const firstValue = Number(this.first?.value)
+        const secondValue = Number(this.second?.value)
 
+        switch (this.choice) {
+            case 1:
+                this.result = firstValue + secondValue
+                break;
+            case 2:
+                this.result = firstValue - secondValue
+                break;
+            case 3:
+                this.result = firstValue * secondValue
+                break;
+            case 4:
+                this.result = firstValue / secondValue
+                break;
+            default:
+                break;
+        }
+    }
 }
