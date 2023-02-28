7774d474aeeebb05116a2f46044250bad7ce980d
Joydip <joydip.mondal@gmail.com>
2023-02-28 16:16:32 +0530

    day-2 Reactive Form Upload

---
+++ codes/day-2/first-ng-app/src/app/calculation/calculation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-calculation',
+    templateUrl: './calculation.component.html',
+    styleUrls: ['./calculation.component.css']
+})
+export class CalculationComponent {
+
+}
